d60ff82173cd762389b23fc76c586f06dd972c82
AMIT SINGH <amitsingh@AMITs-MacBook-Air.local>
2025-03-29 16:09:53 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Let's start JavaScript")
+
+async function getSongs(){
+    let a = await fetch("http://127.0.0.1:5500/songs")
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for(let index = 0; index < as.length; index++){
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href)
+        } 
+    }
+     console.log(songs);
+     
+}
+
+getSongs()
